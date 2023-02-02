f395bd227c4b527c305c5756ed4c671c811c42a7
mgrumbine <you@example.com>
2023-02-01 20:23:14 -0700

    Just added the 4 files under components under search

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ItemService } from 'src/app/services/item.service';
+//will need to create an item service, then import this
+//then, will add item service into constructor and add a getItems method to display on dashboard view
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  
+  constructor(private logoutservice : AuthenticationService, private itemService : ItemService) { }
+  
+  public items;
+
+  ngOnInit() {
+    this.get();
+  }
+
+  get(){
+  this.itemService.getItems().subscribe(
+  data => {this.items = data},
+      err => console.error(err),
+      () => console.log('items loaded')
+     );
+  }
+
+  submitLogout() {
+    console.log("logout works!")
+    this.logoutservice.logout();
+  }
+}
