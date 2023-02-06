19e36c7578b0d153e918be4cb77db7c37d151f9a
mgrumbine <you@example.com>
2023-02-06 16:50:13 -0700

    Functioning table. Edit, delete, display items, and navbar all need some work still.

--- src/app/manufacturer.ts
+++ src/app/manufacturer.ts
@@ -1,7 +1,10 @@
+import { Item } from "./item";
+
 export class Manufacturer {
     id: number = 0;
     name: string = "";
     address: string = "";
     phoneNumber: string = "";
+    items: Item[];
 }
  
